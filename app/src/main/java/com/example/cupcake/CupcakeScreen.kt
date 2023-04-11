f5c7b0f29cc14e3db3afbe31b1af8c6036faaded
AKuan <kuan31045@gmail.com>
2023-04-12 00:31:19 +0800

    Setup NavHost.
    Define routes.
    Create a NavHostController.
    Setup NavHost.

--- app/src/main/java/com/example/cupcake/CupcakeScreen.kt
+++ app/src/main/java/com/example/cupcake/CupcakeScreen.kt
@@ -15,6 +15,7 @@
  */
 package com.example.cupcake
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Scaffold
@@ -26,13 +27,23 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.cupcake.data.DataSource.flavors
+import com.example.cupcake.data.DataSource.quantityOptions
+import com.example.cupcake.ui.OrderSummaryScreen
 import com.example.cupcake.ui.OrderViewModel
+import com.example.cupcake.ui.SelectOptionScreen
+import com.example.cupcake.ui.StartOrderScreen
 
 /**
  * Composable that displays the topBar and displays back button if back navigation is possible.
  */
+
 @Composable
 fun CupcakeAppBar(
     canNavigateBack: Boolean,
@@ -56,8 +67,9 @@ fun CupcakeAppBar(
 }
 
 @Composable
-fun CupcakeApp(modifier: Modifier = Modifier, viewModel: OrderViewModel = viewModel()){
-    // TODO: Create NavController
+fun CupcakeApp(modifier: Modifier = Modifier, viewModel: OrderViewModel = viewModel()) {
+    // TODO: Step2 - Create NavController
+    val navController = rememberNavController()
 
     // TODO: Get current back stack entry
 
@@ -73,7 +85,37 @@ fun CupcakeApp(modifier: Modifier = Modifier, viewModel: OrderViewModel = viewMo
     ) { innerPadding ->
         val uiState by viewModel.uiState.collectAsState()
 
-        // TODO: add NavHost
+        // TODO: Step3 - add NavHost
+        NavHost(
+            navController = navController,
+            startDestination = Screen.Start.name,
+            modifier = modifier.padding(innerPadding)
+        ) {
+            composable(route = Screen.Start.name) {
+                StartOrderScreen(
+                    quantityOptions = quantityOptions
+                )
+            }
+            composable(route = Screen.Flavor.name) {
+                val context = LocalContext.current
+                SelectOptionScreen(
+                    subtotal = uiState.price,
+                    options = flavors.map { id -> context.resources.getString(id) },
+                    onSelectionChanged = { viewModel.setFlavor(it) }
+                )
+            }
+            composable(route = Screen.Pickup.name) {
+                SelectOptionScreen(
+                    subtotal = uiState.price,
+                    options = uiState.pickupOptions,
+                    onSelectionChanged = { viewModel.setDate(it) }
+                )
+            }
+            composable(route = Screen.Summary.name) {
+                OrderSummaryScreen(
+                    orderUiState = uiState
+                )
+            }
+        }
     }
 }
-
---
+++ app/src/main/java/com/example/cupcake/Screen.kt
@@ -0,0 +1,11 @@
+package com.example.cupcake
+
+import androidx.annotation.StringRes
+
+// TODO: Step1 - Define and Create the Screen
+enum class Screen(@StringRes val title: Int) {
+    Start(title = R.string.app_name),
+    Flavor(title = R.string.choose_flavor),
+    Pickup(title = R.string.choose_pickup_date),
+    Summary(title = R.string.order_summary)
+}
